241f7f4caae38514af6106f7a16775bbff5f2ae5
memoc <cobianlg@outlook.com>
2025-03-10 21:15:28 -0600

    2com

--- jscode.js
+++ jscode.js
@@ -1,5 +1,11 @@
-function myFunction() {
+function myFunction1() {
     var x = document.getElementById("myBtn");
     // x.disabled = true;
-    alert("Hello! I am an alert box!!");
+    alert("Hello! funcion 1!!");
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("myBtn");
+    // x.disabled = true;
+    alert("Hello! funcion 2!!");
   }
